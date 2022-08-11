5a1a98a7f0bfcbffa686f7cf63b063fe2392806e
yassh <yassh32@outlook.jp>
2022-08-12 02:05:04 +0900

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["page.js", "page.jsx", "page.ts", "page.tsx"],
+  eslint: {
+    dirs: ["src"],
+  },
+  experimental: {
+    newNextLinkBehavior: true,
+    images: {
+      allowFutureImage: true,
+    },
+  },
 }
 
 module.exports = nextConfig

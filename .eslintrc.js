1f80690745e385f1085b1ee8b6123d01d3ef19dc
yassh <yassh32@outlook.jp>
2022-08-12 01:19:09 +0900

    install and setup eslint-plugin-tailwindcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
     "next/core-web-vitals",
     "prettier",
   ],

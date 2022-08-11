0b34c377d78827d22069d5be0c125b5299811ecd
yassh <yassh32@outlook.jp>
2022-08-12 02:10:40 +0900

    ページ作成＆不要なページ削除

---
+++ src/pages/2022-2023/single/index.page.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next"
+import Head from "next/head"
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>2022-2023 Single Skating</title>
+      </Head>
+      <div>Hello!</div>
+    </>
+  )
+}
+
+export default Page

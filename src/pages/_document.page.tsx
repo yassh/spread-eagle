cffb226f35f4d6f71746377ee9eb2568f12c8438
yassh <yassh32@outlook.jp>
2022-08-12 21:50:43 +0900

    add _document.page.tsx

---
+++ src/pages/_document.page.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html className="text-[clamp(10px,_1vw,_13px)]">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}

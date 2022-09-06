7afc44adc0a8eae568d56d1a396a8b5c14f46710
yassh <yassh32@outlook.jp>
2022-09-06 20:47:39 +0900

    setup Google Analytics

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app"
+import { GoogleAnalytics } from "nextjs-google-analytics"
 import "~/styles/globals.css"
 
 // ☞ https://fontawesome.com/docs/web/use-with/react/use-with#getting-font-awesome-css-to-work
@@ -7,7 +8,12 @@ import "@fortawesome/fontawesome-svg-core/styles.css"
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

115e429e79784dda9525c5fae9018e9f7d52d219
yassh <yassh32@outlook.jp>
2022-09-13 20:38:12 +0900

    マウスホイールで数値が増減するのを防ぐ

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app"
+import { useEffect } from "react"
 import { GoogleAnalytics } from "~/components/GoogleAnalytics"
 import "~/styles/globals.css"
 
@@ -8,6 +9,22 @@ import "@fortawesome/fontawesome-svg-core/styles.css"
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // マウスホイールで数値が増減するのを防ぐ
+    // ☞ https://github.com/facebook/react/issues/24986#issuecomment-1240470376
+    const handleWheel = (event: WheelEvent) => {
+      const target = event.composedPath()[0] as HTMLElement
+      if (target.matches('input[type="number"]')) {
+        target.blur()
+      }
+    }
+
+    document.body.addEventListener("wheel", handleWheel)
+    return () => {
+      document.body.removeEventListener("wheel", handleWheel)
+    }
+  }, [])
+
   return (
     <>
       <GoogleAnalytics />

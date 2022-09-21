72a74f918056881a7ea39d3e39f1da57ef1d9a01
yassh <yassh32@outlook.jp>
2022-09-22 05:28:21 +0900

    引用元更新

--- src/pages/2022-2023/lib/getCalculatedValues/calcElementScore.ts
+++ src/pages/2022-2023/lib/getCalculatedValues/calcElementScore.ts
@@ -19,13 +19,11 @@ export const calcElementScore = (
     const baseValue = getBaseValueOfElement(element.abbr, element.x)
     const goe = getGoeOfElement(element.abbr, element.js)
 
-    // TODO: 2022-2023シーズンの資料からの引用に差し替える
-    //
     // > The panel's score for each Section/Element is determined by
     // > adding the trimmed mean GOE of this Section/Element to its Base
     // > Value.
     //
-    // ☞ https://www.isu.org/inside-isu/rules-regulations/isu-statutes-constitution-regulations-technical/special-regulations-and-technical-rules/17927-single-pair-and-ice-dance-2018/file
+    // ☞ https://isu.org/figure-skating/rules/fsk-regulations-rules/file
     const score = baseValue.plus(goe)
 
     return {

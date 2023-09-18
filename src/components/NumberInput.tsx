9c4ae47f20f2898fa731cb9ac2f5be06a5ad5ea7
yassh <yassh32@outlook.jp>
2023-09-18 18:41:13 +0900

    yarn fix

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -14,26 +14,25 @@ type Props = DetailedHTMLProps<
 
 type Ref = HTMLInputElement
 
-export const NumberInput = forwardRef<Ref, Props>(function NumberInput(
-  props,
-  ref,
-) {
-  const { className, onChange, ...restProps } = props
+export const NumberInput = forwardRef<Ref, Props>(
+  function NumberInput(props, ref) {
+    const { className, onChange, ...restProps } = props
 
-  const debouncedOnChange = useMemo(() => {
-    return onChange && debounceOnChange(onChange)
-  }, [onChange])
+    const debouncedOnChange = useMemo(() => {
+      return onChange && debounceOnChange(onChange)
+    }, [onChange])
 
-  return (
-    <input
-      ref={ref}
-      type="number"
-      className={clsx(
-        "w-full text-end outline outline-1 focus:outline-2",
-        className,
-      )}
-      onChange={debouncedOnChange}
-      {...restProps}
-    />
-  )
-})
+    return (
+      <input
+        ref={ref}
+        type="number"
+        className={clsx(
+          "w-full text-end outline outline-1 focus:outline-2",
+          className,
+        )}
+        onChange={debouncedOnChange}
+        {...restProps}
+      />
+    )
+  },
+)

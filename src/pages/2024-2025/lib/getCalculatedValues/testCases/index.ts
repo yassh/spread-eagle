798be4416caeebb2df3c4ec3133704b42fdef073
yassh <7814130+yassh@users.noreply.github.com>
2024-10-15 21:50:54 +0900

    2023-2024: add test

--- src/pages/2024-2025/lib/getCalculatedValues/testCases/index.ts
+++ src/pages/2024-2025/lib/getCalculatedValues/testCases/index.ts
@@ -3,6 +3,7 @@ import { input as input2, output as output2 } from "./002"
 import { input as input3, output as output3 } from "./003"
 import { input as input4, output as output4 } from "./004"
 import { input as input5, output as output5 } from "./005"
+import { input as input6, output as output6 } from "./006"
 
 export const testCases = [
   { input: input1, output: output1 },
@@ -10,4 +11,5 @@ export const testCases = [
   { input: input3, output: output3 },
   { input: input4, output: output4 },
   { input: input5, output: output5 },
+  { input: input6, output: output6 },
 ]
